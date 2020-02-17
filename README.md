ba71f34dd0f079044a3c0d0593f253c015f4327d
Konstantin Orlov <orlovkn.job@gmail.com>
2020-02-17 07:42:24 +0300

    wip

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# py_learnings
+# lesson 199 Images With Python
+
+## Documentation
+[Pillow Library](https://pillow.readthedocs.io/en/stable)
+
+## Install Pillow
+```
+pip install Pillow
+``` 
--- venv/main.py
+++ venv/main.py
@@ -1,1 +1,23 @@
-print('Hi')
+from PIL import Image, ImageFilter
+
+img = Image.open('./images/pikachu.jpg')
+
+print(img)
+print(img.format)
+print(img.size)
+print(img.mode)
+
+# filtered_img = img.filter(ImageFilter.BLUR) # SHARP
+# filtered_img.save("./images/blur.png", "png")
+
+filtered_img = img.convert("L")
+# filtered_img.save("./images/gray.png", "png")
+# filtered_img.show()
+
+# rotate
+# crocked = filtered_img.rotate(90)
+# crocked.save("./images/crocked.png", "png")
+
+# resize
+resze = filtered_img.resize((300, 300))
+resze.save("./images/resize.png", "png")
