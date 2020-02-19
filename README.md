4211bee3c28a3d7c72425a807281e04945e203aa
Konstantin Orlov <orlovkn.job@gmail.com>
2020-02-19 06:48:10 +0300

    wip

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-## Complete Python Developer in 2020: Zero to Mastery
-
-[Link to the course](https://www.udemy.com/course/complete-python-developer-zero-to-mastery/)
-
-## Instructions
-All exercises are performed in the branches
+## 210 Sending Emails With Python
--- venv/main.py
+++ venv/main.py
@@ -1,1 +1,34 @@
-print('Hi')
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = "Konstantin Orlov"
+email['to'] = "example@ya.ru"
+email['subject'] = "You won a tonn of money"
+
+email.set_content(html.substitute({'name' : 'Alexey'}), 'html')
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('example@gmail.com', 'password')
+    smtp.send_message(email)
+    print('done')
+
+# ======================
+# email = EmailMessage()
+# email['from'] = "Konstantin Orlov"
+# email['to'] = "example@ya.ru"
+# email['subject'] = "You won a tonn of money"
+#
+# email.set_content('Message of the letter')
+#
+# with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+#     smtp.ehlo()
+#     smtp.starttls()
+#     smtp.login('example@gmail.com', 'password')
+#     smtp.send_message(email)
+#     print('done')
